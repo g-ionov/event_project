98cdebd7c13c3ff3e131b086f093a2b4039d7f09
g-ionov <g.ionoff@yandex.ru>
2023-05-29 20:33:05 +0300

    Added JWT auth as DEFAULT_AUTHENTICATION_CLASSES

--- event_service/config/settings.py
+++ event_service/config/settings.py
@@ -86,8 +86,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',),
 }
 
 SWAGGER_SETTINGS = {
